b35b0ab1932c84f22ef01b358de1557c7e752746
Brenner-Costa <Costabrenner1@gmail.com>
2022-06-05 13:48:36 -0300

    adiciona footer, navbar e sidebar no include // card 3D implementado

---
+++ app/views/includes/navbar.view.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../../public/css/style_navbar.css" type="text/css">
+
+    <title>Banhato's Pizza</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-md fixed-top navigation">
+        <div class="container-fluid">
+            <a clasrs="navbar-brand m" hef="#">
+                <img src="../../../public/img/Icone Marrom Circular sem Fundo.png" alt="" width="110" height="110" class="nav-img">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon "></span>
+        </button>
+
+            <div class="collapse navbar-collapse justify-content-between" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.view.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="produtos.view.php">Produtos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="quem-somos.view.php">Quem somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contato.view.php">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.view.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
