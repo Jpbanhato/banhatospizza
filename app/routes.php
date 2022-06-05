8139eed3ed37755a000d0938dac7caaf42f18b7c
GuCMartins <coelhomartins7@gmail.com>
2022-06-05 18:45:14 -0300

    reconstrucao visual dos produtos

--- app/routes.php
+++ app/routes.php
@@ -4,39 +4,30 @@
  * Categorias:
  */
   $router->post("admin/categorias/create", "CategoriasController@create");
-
   $router->get("admin/categorias", "CategoriasController@index");
   $router->get("admin/categorias/result", "CategoriasController@show");
-
   $router->post("admin/categorias/update", "CategoriasController@update");
-  
   $router->post("admin/categorias/delete", "CategoriasController@delete");
 /**
  * Produtos:
  */
+  $router->get('admin/produtos','ProdutosAdmController@index');
+  $router->post('admin/produtos/create','ProdutosAdmController@create');
+  $router->post('admin/produtos/delete','ProdutosAdmController@delete');
+  $router->post('admin/produtos/update','ProdutosAdmController@update');
+  $router->get('admin/produtos/search','ProdutosAdmController@search');
+
 /**
  * Contato:
  */
 $router ->get('contato','ContatoController@view');
 $router ->post('contato/sendEmail', 'ContatoController@sendEmail');
-
  $router->get('admin/usuarios','UserController@view');
-
  $router->post('admin/usuarios/create','UserController@create');
-
  $router->post('admin/usuarios/update','UserController@update');
-
  $router->post('admin/usuarios/delete','UserController@delete');
-
  $router->post('admin/usuarios/select','UserController@select');
-
  $router->get('admin/usuarios/search','UserController@search');
-
-$router->get('admin/produtos','ProdutosAdmController@index');
-$router->post('admin/produtos/create','ProdutosAdmController@create');
-$router->post('admin/produtos/delete','ProdutosAdmController@delete');
-$router->post('admin/produtos/update','ProdutosAdmController@update');
-$router->get('admin/produtos/search','ProdutosAdmController@search');
 
 
 /**
--- app/views/site/produtos.view.php
+++ app/views/site/produtos.view.php
@@ -9,192 +9,112 @@
     <title>Banhato's Pizza - Conheça sabores inesquecíveis</title>
 </head>
 <body>
-    <div class="main">
-        <div class="indicacao">
-          <h1>Produtos</h1>
-          <nav class="navbar nav">
-            <div class="container-fluid cont">
-              <form class="d-flex filter">
-                <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
-                <button class="btn btn-outline-success bot" type="submit">Buscar</button>
-              </form>
-            </div>
-          </nav>
+  <div class="main">
+    <div class="indicacao">
+      <h1>Produtos</h1>
+      <nav class="navbar nav">
+        <div class="container-fluid cont">
+           <form class="d-flex filter">
+            <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
+            <button class="btn btn-outline-success bot" type="submit">Buscar</button>
+          </form>
         </div>
-        <div class="container">
-            <div class="produto p1">
-             <a href="#">
-               <img src="../../../public/img/batata-frita.jpg">
-               <div class="produto_txt">
-               <h1>Batatas Fritas</h1>
-              </a>
-              <a href="#">
-                <div class="botao entrada" >
-                 <p>Entrada</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
+      </nav>
+    </div>
+    <div class="container">
+      <div class="row row-cols-3 row-cols-md-4 g-4">
+        <div class="col card">
+          <div class="card h-100 produto">
+            <img src="../../../public\img\batata-frita.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <div class="botao" style="background-color: rgb(158, 148, 62);">
+                <a href="#">Porções</a>
               </div>
-            </div> 
-          </div>
-            <div class="produto p2">
-              <a href="#">
-                <img src="../../../public/img/bruschetta-burrata.jpg">
-                <div class="produto_txt">
-                <h1>Bruschetta de Burrata</h1>
-              </a>
-              <a href="#">
-                <div class="botao porcao" >
-                 <p>Porção</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
-              </div>
-            </div> 
-          </div>
-            <div class="produto p3">
-              <a href="#">
-                <img src="../../../public/img/pizza.jpg">
-                <div class="produto_txt">
-                <h1>Pizza de Margherita</h1>
-              </a>
-              <a href="#">
-                <div class="botao pizza" >
-                 <p>Pizza</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
-              </div>
-            </div> 
-          </div>
-            <div class="produto p4">
-              <a href="#">
-                <img src="../../../public/img/calzone-pizza.jpg">
-                <div class="produto_txt">
-                <h1>Calzone Misto</h1>
-              </a>
-              <a href="#">
-                <div class="botao calzone" >
-                 <p>Calzone</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
-              </div>
-            </div> 
-          </div>
-            <div class="produto p5">
-              <a href="#">
-                <img src="../../../public/img/panna-cotta.jpg">
-                <div class="produto_txt">
-                <h1>Panna Cotta</h1>
-              </a>
-              <a href="#">
-                <div class="botao sobremesa">
-                  <p>Sobremesa</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
-              </div>
-            </div> 
-          </div>
-            <div class="produto p6">
-              <a href="#">
-                <img src="../../../public/img/expresso.jpg">
-                <div class="produto_txt">
-                <h1>Expresso Maltado</h1>
-              </a>
-              <a href="#">
-                <div class="botao cafe">
-                  <p>Café</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
-              </div>
-            </div> 
-          </div>
-            <div class="produto p7">
-              <a href="#">
-                <img src="../../../public/img/limoncello.jpg">
-                <div class="produto_txt">
-                <h1>Limoncello</h1>
-              </a>
-              <a href="#">
-                <div class="botao digestivos">
-                  <p>Digestivo</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
-              </div>
-            </div> 
-          </div>
-            <div class="produto p8">
-              <a href="#">
-                <img src="../../../public/img/queijos-frutas.jpg">
-                <div class="produto_txt">
-                <h1>Tabua de frutas e queijos</h1>
-              </a>
-              <a href="#">
-                <div class="botao queijos-frutas">
-                  <p>Queijos / Frutas</p>
-                </div>
-              </a>
-                <div class="desc">   
-                  <p>Descrição, ingredientes, preparo</p>
-                  <p>Preço/delivery</p>
-                </div>
-              </div> 
+              <h1 class="card-title">Batata Frita</h1>
+              <p class="card-text">Preco</p>
             </div>
-            <div class="produto p9">
-              <a href="#">
-                <img src="../../../public/img/vinho.jpg">
-                <div class="produto_txt">
-                <h1>Vinho</h1>
-              </a>
-              <a href="#">
-                <div class="botao bebida">
-                  <p>Bebida</p>
-                </div>
-              </a>
-              <div class="desc">   
-                <p>Descrição, ingredientes, preparo</p>
-                <p>Preço/delivery</p>
-              </div>
-            </div> 
           </div>
-          <nav aria-label="Page navigation example">
-            <ul class="pagination pagina">
-              <li class="page-item pag-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                </a>
-              </li>
-              <li class="page-item pag-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item pag-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item pag-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item pag-item">
-                <a class="page-link" href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+        </div>
+        <div class="col card">
+          <div class="card h-100 produto">
+            <img src="../../../public\img\bruschetta-burrata.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <div class="botao" style="background-color: #8f0e0e;">
+                <a href="#">Entradas</a>
+              </div>
+              <h1 class="card-title">Bruschetta com Burrata</h1>
+              <p class="card-text">Preco</p>
+            </div>
+          </div>
+        </div>
+        <div class="col card">
+          <div class="card h-100 produto">
+            <img src="../../../public\img\pizza.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <div class="botao " style="background-color: #cf6a18b7;">
+                <a href="#">Pizzas</a>
+              </div>
+              <h1 class="card-title">Pizza de Calabresa</h1>
+              <p class="card-text">Preco</p>
+            </div>
+          </div>
+        </div>
+        <div class="col card">
+        <div class="card h-100 produto">
+            <img src="../../../public\img\calzone-pizza.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <div class="botao " style="background-color: #755000;">
+                <a href="#">Calzones</a>
+              </div>
+              <h1 class="card-title">Calzone de Queijo</h1>
+              <p class="card-text">Preco</p>
+            </div>
+          </div>
+        </div>
+        <div class="col card">
+          <div class="card h-100 produto">
+            <img src="../../../public\img\panna-cotta.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <div class="botao " style="background-color: rgb(201, 85, 104);">
+                <a href="#">Sobremesas</a>
+              </div>
+              <h1 class="card-title">Panna Cotta</h1>
+              <p class="card-text">Preco</p>
+            </div>
+          </div>
+        </div>
+        <div class="col card">
+          <div class="card h-100 produto">
+            <img src="../../../public\img\vinho.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <div class="botao " style="background-color: rgb(68, 5, 65);">
+                <a href="#">Vinhos</a>
+              </div>
+              <h1 class="card-title">Vinho</h1>
+              <p class="card-text">Preco</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>    
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>   
+    </div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination pagina">
+        <li class="page-item item-pag">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+         </a>
+        </li>
+        <li class="page-item item-pag"><a class="page-link" href="#">1</a></li>
+       <li class="page-item item-pag"><a class="page-link" href="#">2</a></li>
+        <li class="page-item item-pag"><a class="page-link" href="#">3</a></li>
+        <li class="page-item item-pag">
+          <a class="page-link item-pag" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>    
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>   
 </body>
 </html>
