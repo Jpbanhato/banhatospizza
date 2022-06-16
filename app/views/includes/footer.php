c3b6f4e8219846fd4c1393d5cf7adbbaeddd3236
Beortizz <beortizz@gmail.com>
2022-06-15 22:28:37 -0300

    navbar, footer e home corrigidas

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,101 +1,84 @@
-
-    <!-- Footer -->
-    <div class="container-fluid">
-        <div class="row">
-            <!-- Footer -->
-            <footer class="text-center text-lg-start">
-                <!-- Grid container -->
-                <div class="container p-4 pb-0">
-                    <!-- Section: Links -->
-                    <section class="">
-                        <!--Grid row-->
-                        <div class="row justify-content-center">
-                            <!--Grid column-->
-                            <div class="col-md-3">
-                                <h5 class="text-uppercase titulo">Banhato's Pizza</h5>
-
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae modi cum ipsam ad, illo possimus laborum ut reiciendis obcaecati. Ducimus, quas. Corrupti, pariatur eaque? Reiciendis assumenda iusto sapiente inventore animi?
-                                </p>
-                            </div>
-                            <!--Grid column-->
-                            <div class="col-md-6">
-                                <center>
-                                    <img src="../../../public/img/Icone Marrom Circular sem Fundo.png" alt="" height="300" width="300">
-                                </center>
-                            </div>
-                            <!--Grid column-->
-                            <div class="col-md-3">
-                                <h5 class="text-uppercase">Menu</h5>
-
-                                <ul class="list-unstyled mb-0">
-                                    <li>
-                                        <a href="/" class="footer-link">Home</a>
-                                    </li>
-                                    <li>
-                                        <a href="/produtos" class="footer-link">Produtos</a>
-                                    </li>
-                                    <li>
-                                        <a href="/quem-somos" class="footer-link">Quem Somos</a>
-                                    </li>
-                                    <li>
-                                        <a href="/contato" class="footer-link">Contato</a>
-                                    </li>
-                                    <li>
-                                        <a href="/login" class="footer-link">Login</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!--Grid row-->
-                    </section>
-                    <!-- Section: Links -->
-
-                    <hr class="mb-4" />
-
-                    <!-- Section: CTA -->
-                    <section class="">
-                        <p class="d-flex justify-content-center align-items-center">
-                            <span class="me-3">Registre-se agora</span>
-                            <button type="button" class="btn btn-outline-light btn-rounded footer-btn">
-                                Registrar-se
-                                
-                </button>
-                        </p>
-                    </section>
-                    <!-- Section: CTA -->
-
-                    <hr class="mb-4" />
-
-                    <!-- Section: Social media -->
-                    <section class="mb-4 text-center">
-                        <!-- Facebook -->
-                        <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i class="fa fa-facebook-f"></i></a>
-
-                        <!-- Twitter -->
-                        <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i class="fa fa-twitter"></i></a>
-
-                        <!-- Google -->
-                        <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i class="fa fa-google"></i></a>
-
-                        <!-- Instagram -->
-                        <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i class="fa fa-instagram"></i></a>
-
-                    </section>
-                    <!-- Section: Social media -->
-                </div>
-                <!-- Grid container -->
-
-                <!-- Copyright -->
-                <div class="row">
-                    <div class="text-center p-3">
-                        © 2022 Copyright:
-                        <a class="text-white footer-link" href="https://mdbootstrap.com/">Codejr.com.br</a
-              >
-                    </div>
-          </div>
-          <!-- Copyright -->
-        </footer>
-        </div>
+<!-- Footer -->
+<div class="container-fluid">
+    <div class="row">
         <!-- Footer -->
-      </div>
+        <footer class="text-center text-lg-start">
+            <!-- Grid container -->
+            <div class="container p-4 pb-0">
+                <!-- Section: Links -->
+                <section class="">
+                    <!--Grid row-->
+                    <div class="row justify-content-center footer-container">
+                        <!--Grid column-->
+                        <div class="col-md-3">
+                            <h5 class="text-uppercase titulo">Banhato's Pizza</h5>
+
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae modi cum ipsam ad,
+                                illo possimus laborum ut reiciendis obcaecati. Ducimus, quas. Corrupti, pariatur eaque?
+                                Reiciendis assumenda iusto sapiente inventore animi?
+                            </p>
+                        </div>
+                        <!--Grid column-->
+                        <div class="col-md-6">
+                            <center>
+                                <img src="../../../public/img/Icone Marrom Circular sem Fundo.png" alt="" height="300"
+                                    width="300">
+                            </center>
+                        </div>
+                        <!--Grid column-->
+                        <div class="col-md-3">
+                            <h5 class="text-uppercase">Menu</h5>
+
+                            <ul class="list-unstyled mb-0">
+                                <li>
+                                    <a href="/" class="footer-link">Home</a>
+                                </li>
+                                <li>
+                                    <a href="/produtos" class="footer-link">Produtos</a>
+                                </li>
+                                <li>
+                                    <a href="/quem-somos" class="footer-link">Quem Somos</a>
+                                </li>
+                                <li>
+                                    <a href="/contato" class="footer-link">Contato</a>
+                                </li>
+                                <li>
+                                    <a href="/login" class="footer-link">Login</a>
+                                </li>
+                            </ul>
+                            <br />
+                            <!-- Section: Social media -->
+                            <section class="mb-4 text-left">
+                                <!-- Facebook -->
+                                <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i
+                                        class="fa fa-facebook-f fa-footer"></i></a>
+
+                                <!-- Twitter -->
+                                <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i
+                                        class="fa fa-twitter fa-footer"></i></a>
+
+                                <!-- Google -->
+                                <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i
+                                        class="fa fa-google fa-footer"></i></a>
+
+                                <!-- Instagram -->
+                                <a class="btn btn-outline-light btn-floating m-1 footer-icon" href="#!" role="button"><i
+                                        class="fa fa-instagram fa-footer"></i></a>
+
+                            </section>
+                            <!-- Section: Social media -->
+                        </div>
+                    </div>
+                    <!--Grid row-->
+                </section>
+                <!-- Section: Links -->
+
+
+            </div>
+            <!-- Grid container -->
+
+        </footer>
+    </div>
+    <!-- Footer -->
+</div>
--- app/views/includes/navbar.php
+++ app/views/includes/navbar.php
@@ -1,32 +1,32 @@
-
 <nav class="navbar navbar-expand-md fixed-top navigation">
-        <div class="container-fluid">
-            <a clasrs="navbar-brand m" hef="#">
-                <img src="../../../public/img/Icone Marrom Circular sem Fundo.png" alt="" width="110" height="110" class="nav-img">
-            </a>
+    <div class="container-fluid">
+        <a clasrs="navbar-brand m" hef="/">
+            <img src="../../../public/img/Icone Marrom Circular sem Fundo.png" alt="" width="110" height="110"
+                class="nav-img">
+        </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon "></span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon "></span>
         </button>
 
-            <div class="collapse navbar-collapse justify-content-between" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/produtos">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/quem-somos">Quem somos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/contato">Contato</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/login">Login</a>
-                    </li>
-                </ul>
-            </div>
+        <div class="collapse navbar-collapse justify-content-between" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/produtos">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/quem-somos">Quem somos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contato">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
