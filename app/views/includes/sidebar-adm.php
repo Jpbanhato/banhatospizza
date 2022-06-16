92f2bcee2230c1d03928476e2816ceea84e66ec2
Beortizz <beortizz@gmail.com>
2022-06-15 21:11:14 -0300

    sidebar e telas adm finalizadas

--- app/views/includes/sidebar-adm.php
+++ app/views/includes/sidebar-adm.php
@@ -1,47 +1,28 @@
+<div class="col-auto col-md-3 col-xl-2 px-3 px-sm-2 pt-2 styled-sidebar">
+    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
 
-  <div class="botao-primario1">
-    <a class="btn btn-primary botao-primario2" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
-      aria-controls="offcanvasExample"> <i class="bi bi-list"></i> </a>
-  </div>
-
-
-  <div class="container flexbox_cont">
-
-    <div class="offcanvas offcanvas-start inicio-sidebar" tabindex="-1" id="offcanvasExample"
-      aria-labelledby="offcanvasExampleLabel">
-      <div class="offcanvas-header titulo-topo">
-        <img src="/public/assets/Icone Redondo sem Fundo.png">
-        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Banhatos Pizza</h5>
-        <button type="button" class="btn-close text-reset botao-fechar" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-
-      </div>
-
-      <div class="offcanvas-body corpo-sidebar">
-
-        <ul class="listas">
-
-          <li> <i class="bi bi-people"></i> <a class="usuarios-1" href="/admin/usuarios"> Usuários </a></li>
-          <li> <i class="bi bi-list-nested"></i> <a class="categorias-1" href="/admin/categorias"> Categorias </a></li>
-          <li> <i class="bi bi-boxes"></i> <a class="produtos-1" href="/admin/produtos"> Produtos </a></li>
-
-
-        </ul>
-
-        <div class="botao-logout1">
-          <!-- Button trigger modal -->
-          <form method="post" action="/logout">
-              <button type="submit" class="btn btn-primary botao-logout2" data-bs-toggle="modal" data-bs-target="#modalLogout">
-                Logout <i class="bi bi-box-arrow-right"></i>
-              </button>
-          </form> 
-        </div>
-
-    </div>
-    </div>
-  </div>
-  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
-  
-
+        <li class="nav-item">
+            <a href="/admin/dashboard" class="nav-link align-middle px-0">
+                <i class="bi bi-house-fill"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+            </a>
+        </li>
+        <li>
+            <a href="/admin/categorias" class="nav-link px-0 align-middle">
+                <i class="bi bi-clipboard-data-fill"></i> <span class="ms-1 d-none d-sm-inline">Categorias</span></a>
+        </li>
+        <li>
+            <a href="/admin/produtos" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                <i class="bi bi-boxes"></i> <span class="ms-1 d-none d-sm-inline">Produtos</span> </a>
+        </li>
+        <li>
+            <a href="/admin/usuarios" class="nav-link px-0 align-middle">
+                <i class="bi bi-people-fill"></i> <span class="ms-1 d-none d-sm-inline">Usuários</span>
+            </a>
+        </li>
+    </ul>
+    <form action="/logout" method="post">
+        <button type="submit" href="" class="nav-link px-0 align-middle nav-button">
+            <i class="bi bi-box-arrow-right"></i> <span class="ms-1 d-none d-sm-inline">Encerrar Sessão</span>
+        </button>
+    </form>
+</div>
